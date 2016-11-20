f50dc0131bcd4246296500c330d282556c085aac
Rootie <mack0r3@gmail.com>
2016-11-20 16:18:45 +0000

    Fixed UUID

--- src/AcceptConnectionThread.java
+++ src/AcceptConnectionThread.java
@@ -5,28 +5,30 @@ import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
 import javax.microedition.io.StreamConnectionNotifier;
 
+import java.util.*;
+
+
+
 public class AcceptConnectionThread extends Thread {
-	
+
 	private static final String serviceName = "RpiService";
-	private static int serviceUUID = 9078267;
-			
+	private static final String uuid = "1ea6b38800001000800000805f9b34fb";
+
 	public AcceptConnectionThread() {
-		
+
 	}
-	
+
 	@Override
 	public void run() {
 		acceptConnection();
 	}
-	
+
 	private void acceptConnection() {
 		StreamConnectionNotifier notifier = null;
 		StreamConnection connection = null;
-		
 		try {
 			LocalDevice localDevice = LocalDevice.getLocalDevice();
 			localDevice.setDiscoverable(DiscoveryAgent.GIAC);
-			
 			String connectionURL = createConnectionURL();
 			notifier = (StreamConnectionNotifier)Connector.open(connectionURL);
 		} catch (Exception ex) {
@@ -35,16 +37,16 @@ public class AcceptConnectionThread extends Thread {
 			ex.printStackTrace();
 			return;
 		}
-		
+
 		while(true) {
 			try {
 				Log("Waiting for bluetooth connection...");
-				connection = notifier.acceptAndOpen();				
+				connection = notifier.acceptAndOpen();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 				return;
 			}
-			
+
 			if (connection != null) {
 				Log("Connection established...");
 				Thread thread = new Thread(new ProcessConnectionThread(connection));
@@ -55,18 +57,17 @@ public class AcceptConnectionThread extends Thread {
 					Log("Error closing bluetooth stream");
 					ex.printStackTrace();
 				}
-				
+
 				return;
 			}
 		}
-									
+
 	}
-	
+
 	private String createConnectionURL() {
-		UUID uuid = new UUID(serviceUUID);
-		return "btspp://localhost:" +  uuid.toString() + ";name=" + serviceName;
+		return "btspp://localhost:" + uuid + ";name=" + serviceName;
 	}
-	
+
 	private static void Log(String message) {
 		System.out.println(message);
 	}
