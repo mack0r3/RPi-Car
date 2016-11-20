e7d25c17b99e7bb04870ab28e03ee25412497e71
Korzonkie <mack0r3@gmail.com>
2016-11-20 01:05:02 +0100

    Initial commit

---
+++ src/BluetoothServer.java
@@ -0,0 +1,12 @@
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.UUID;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
+
+public class BluetoothServer {
+	public static void main(String[] args) {
+		
+	}
+}
