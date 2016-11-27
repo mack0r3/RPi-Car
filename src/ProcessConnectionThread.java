c57f2aa6c0436a34c8a06dac37986b83a112ba47
Rootie <mack0r3@gmail.com>
2016-11-27 14:04:20 +0000

    Handling Led status

--- src/ProcessConnectionThread.java
+++ src/ProcessConnectionThread.java
@@ -1,24 +1,45 @@
+ import com.pi4j.io.gpio.*;
+
 import javax.microedition.io.StreamConnection;
 import java.io.InputStream;
 
 public class ProcessConnectionThread extends Thread{
-	
+
 	private StreamConnection connection;
-	
+	private final GpioPinDigitalOutput pin;
+
 	public ProcessConnectionThread(StreamConnection connection) {
 		this.connection = connection;
+
+		GpioController gpio = GpioFactory.getInstance();
+		pin = gpio.provisionDigitalOutputPin(
+			RaspiPin.GPIO_01, "LED", PinState.HIGH);
+		pin.setShutdownOptions(true, PinState.LOW);
 	}
-	
+
 	@Override
 	public void run() {
-		InputStream inputStream = connection.openInputStream();
+		InputStream inputStream = null;
+		try {
+			inputStream = connection.openInputStream();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		System.out.println("Waiting for inputStream");
 		while(true) {
-			int ledStatus = inputStream.read();
+			int ledStatus = -1;
+			try {
+				ledStatus = inputStream.read();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 			System.out.println("Led status: " + ledStatus);
 			if(ledStatus == 1) {
-				//turn led on
+				System.out.println("ON");
+				pin.high();
 			} else {
+				System.out.println("OFF");
+				pin.low();
 				//turn led off
 			}
 		}
