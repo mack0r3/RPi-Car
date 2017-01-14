93c44575bd24bce5f4ca6514931d21967cf54f49
Rootie <mack0r3@gmail.com>
2017-01-14 17:32:07 +0100

    Receiving signal and making appropiate motors spin behaviour

--- src/AcceptConnectionThread.java
+++ src/AcceptConnectionThread.java
@@ -11,64 +11,64 @@ import java.util.*;
 
 public class AcceptConnectionThread extends Thread {
 
-	private static final String serviceName = "RpiService";
-	private static final String uuid = "1ea6b38800001000800000805f9b34fb";
+    private static final String serviceName = "RpiService";
+    private static final String uuid = "1ea6b38800001000800000805f9b34fb";
 
-	public AcceptConnectionThread() {
+    public AcceptConnectionThread() {
 
-	}
+    }
 
-	@Override
-	public void run() {
-		acceptConnection();
-	}
+    @Override
+    public void run() {
+        acceptConnection();
+    }
 
-	private void acceptConnection() {
-		StreamConnectionNotifier notifier = null;
-		StreamConnection connection = null;
-		try {
-			LocalDevice localDevice = LocalDevice.getLocalDevice();
-			localDevice.setDiscoverable(DiscoveryAgent.GIAC);
-			String connectionURL = createConnectionURL();
-			notifier = (StreamConnectionNotifier)Connector.open(connectionURL);
-		} catch (Exception ex) {
-			Log("Error opening bluetooth connection");
-			Log(createConnectionURL());
-			ex.printStackTrace();
-			return;
-		}
+    private void acceptConnection() {
+        StreamConnectionNotifier notifier = null;
+        StreamConnection connection = null;
+        try {
+            LocalDevice localDevice = LocalDevice.getLocalDevice();
+            localDevice.setDiscoverable(DiscoveryAgent.GIAC);
+            String connectionURL = createConnectionURL();
+            notifier = (StreamConnectionNotifier)Connector.open(connectionURL);
+        } catch (Exception ex) {
+            Log("Error opening bluetooth connection");
+            Log(createConnectionURL());
+            ex.printStackTrace();
+            return;
+        }
 
-		while(true) {
-			try {
-				Log("Waiting for bluetooth connection...");
-				connection = notifier.acceptAndOpen();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				return;
-			}
+        while(true) {
+            try {
+                Log("Waiting for bluetooth connection...");
+                connection = notifier.acceptAndOpen();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return;
+            }
 
-			if (connection != null) {
-				Log("Connection established...");
-				Thread thread = new Thread(new ProcessConnectionThread(connection));
-				thread.start();
-				try {
-					notifier.close();
-				} catch (Exception ex) {
-					Log("Error closing bluetooth stream");
-					ex.printStackTrace();
-				}
+            if (connection != null) {
+                Log("Connection established...");
+                Thread thread = new Thread(new ProcessConnectionThread(connection));
+                thread.start();
+                try {
+                    notifier.close();
+                } catch (Exception ex) {
+                    Log("Error closing bluetooth stream");
+                    ex.printStackTrace();
+                }
 
-				return;
-			}
-		}
+                return;
+            }
+        }
 
-	}
+    }
 
-	private String createConnectionURL() {
-		return "btspp://localhost:" + uuid + ";name=" + serviceName;
-	}
+    private String createConnectionURL() {
+        return "btspp://localhost:" + uuid + ";name=" + serviceName;
+    }
 
-	private static void Log(String message) {
-		System.out.println(message);
-	}
+    private static void Log(String message) {
+        System.out.println(message);
+    }
 }
